4790f30159fdac50e6d809d41226b8484c9824e0
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-26 13:18:29 +0530

    Create setInterval.js

---
+++ setInterval.js
@@ -0,0 +1,10 @@
+
+console.log("Just like setTimeout this method is also similar only difference is it runs continously after the set Time Interval")
+
+
+setInterval(()=>{
+
+  console.log("this msg will show after every 1 second!!")
+
+
+},1000)
