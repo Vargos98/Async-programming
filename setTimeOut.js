92e9d1708f906564aa6b4184ff846b5851e8f982
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-26 13:16:26 +0530

    Create setTimeOut.js

---
+++ setTimeOut.js
@@ -0,0 +1,6 @@
+
+
+console.log("the code tends to after its time is completed then only the setTimeout method implements the function inside it.")
+setTimeout(function(){
+  console.log("The code runs after its time is completed, hence making it asynchronous in nature.")
+},3000)
