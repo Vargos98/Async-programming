696b0726ff0dba70b26b7109ab37ad02b88b061d
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-27 14:01:43 +0530

    Implemented swap imgs functionality

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+// const btn = document.querySelector('#btn')
+// const para = document.querySelector('.para')
+// btn.addEventListener('click', function(){
+//   para.textContent = "Lorem87";
+// })
+
+
+
+let img1 = document.querySelector("#one");
+let img2 = document.querySelector("#two");
 const btn = document.querySelector('#btn')
-const para = document.querySelector('.para')
+
 btn.addEventListener('click', function(){
-  para.textContent = "Lorem87";
+  var src1 = img1.src;
+  var src2 = img2.src;
+
+  img1.src = src2;
+  img2.src = src1;
+
 })
